1aa5093df496ae33855f47b307f77788687d1fac
hassan.adel <hasanadel555@hotmail.com>
2020-04-06 22:30:51 +0200

    Creating a Stream

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -5,12 +5,16 @@ import com.tutorial.lambdas.Printer;
 import com.tutorial.lambdas.ConsolePrinter;
 import com.tutorial.streams.StreamsDemo;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         StreamsDemo.show();
+        StreamsDemo.CreateStreams();
     }
 }
--- src/com/tutorial/streams/StreamsDemo.java
+++ src/com/tutorial/streams/StreamsDemo.java
@@ -1,6 +1,9 @@
 package com.tutorial.streams;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamsDemo {
     public static void show(){
@@ -12,5 +15,21 @@ public class StreamsDemo {
 
         var count = movies.stream().filter(i -> i.getLikes()>10).count();
         System.out.println(count);
+    }
+    public static void CreateStreams(){
+        Collection<Integer> test = List.of(1,23,63,7,4,2);
+        var stream = test.stream();
+
+        int[] arr = {1,3,52,6};
+        var stream2 = Arrays.stream(arr);
+        stream2.forEach(n -> System.out.println(n));
+
+        Stream.of(1,5,34,768,3);
+
+        var infiniteStream =  Stream.generate(()->Math.random());
+        infiniteStream.limit(10).forEach(n-> System.out.println(n));
+
+        var infiniteStream2 = Stream.iterate(1, n->n+2);
+        infiniteStream2.limit(3).forEach(n-> System.out.println(n));
     }
 }
